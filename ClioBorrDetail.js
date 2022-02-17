d99b144ce65080ce598b9fc13d18d78eef7b5f1a
kvance60115 <kendall@vancenet.net>
2022-02-17 11:14:05 -0600

    working on nested selectors - not oving it

--- ClioBorrDetail.js
+++ ClioBorrDetail.js
@@ -12,8 +12,13 @@
 // var pickup_location=document.querySelector("#reqcollapse > div:nth-child(1) > div.col-md-6 > dl > dd:nth-child(36) > b").innerHTML;
 var pickup_location=document.querySelector("#reqcollapse > div:nth-child(1) > div.col-md-6 > dl > dd:nth-child(32) > b").innerHTML;
 var name=document.querySelector("#patronHold453 > div > div.col-md-9 > dl > dd:nth-child(2)")
-alert(pickup_location);
+
+var selTest = document.querySelector("#reqcollapse > div:nth-child(1) > div.col-md-6");
+var selDd = selTest
+
+//alert(pickup_location);
 console.log(name);
+alert(selDd.innerHTML);
 
 
 
