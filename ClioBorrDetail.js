4126ce6bb1955774ceb12987bd0115c67ee5274b
kvance60115 <kendall@vancenet.net>
2022-02-17 16:14:01 -0600

    got the llabel working. next is to get  loc

--- ClioBorrDetail.js
+++ ClioBorrDetail.js
@@ -2,8 +2,8 @@
 // @name         ClioBorrDet
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the Clio!
-// @author       Kendall Vance
+// @description  try to take over the world!
+// @author       You
 // @match        https://waubonsee-staff.cliohosting.com/BorRequest/Details/*
 // @icon         https://www.google.com/s2/favicons?domain=cliohosting.com
 // @grant        none
@@ -19,19 +19,30 @@ var selDd = selTest; */
 
 //alert(pickup_location);
 var sel = document.querySelectorAll("#reqcollapse > div:nth-child(1) > div.col-md-6 > dl > dt");
-
+//alert(sel[5].innerHTML);
+var elemnum=-1;
 const re = /.*location.*/i;
 var mystr = "";
-for (const e of sel) {
-         mystr =e.innerHTML;
-          console.log(mystr);
+var label = "";
+var name ="";
+var pu = "";
 
-        //this was the hard part
+
+for (const e of sel) {
+        elemnum++;
+         mystr =e.innerHTML;
+          console.log(elemnum);
+          console.log(mystr);
+label = sel[elemnum].innerHTML;
+    //location = sel[elemnum].;
+
        console.log(re.test(mystr));
+    if(re.test(mystr)){
+        alert(label);
+    }
 
 }
-const pattern = /e/;
-pattern.test("The best things in life are free!");
+
 
 (function() {
     'use strict';
